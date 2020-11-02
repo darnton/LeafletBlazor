b9ad60bda9be5217cdae2c4065f45e1602cc0f78
Bernard Darnton <bernard@darnton.co.nz>
2020-11-02 21:22:42 +1300

    Add test rig project

---
+++ LeafletBlazorTestRig/Pages/Index.razor.cs
@@ -0,0 +1,31 @@
+﻿using Darnton.Blazor.Leaflet.LeafletMap;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace LeafletBlazorTestRig.Pages
+{
+    public class IndexBase : ComponentBase
+    {
+        [Inject] public IJSRuntime JSRuntime { get; set; }
+
+        protected Map PositionMap;
+        protected TileLayer OpenStreetMapsTileLayer;
+
+        public IndexBase() : base()
+        {
+            PositionMap = new Map("testMap", new MapOptions //Centred on New Zealand
+            {
+                Center = new LatLng(-42, 175),
+                Zoom = 4
+            });
+            OpenStreetMapsTileLayer = new TileLayer(
+                "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                new TileLayerOptions
+                {
+                    Attribution = @"Map data &copy; <a href=""https://www.openstreetmap.org/"">OpenStreetMap</a> contributors, " +
+                        @"<a href=""https://creativecommons.org/licenses/by-sa/2.0/"">CC-BY-SA</a>"
+                }
+            );
+        }
+    }
+}
